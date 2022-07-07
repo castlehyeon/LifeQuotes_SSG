78d7767c429f71e2734920e0de1b1f187cbcd147
castlehyeon <qnftktls118@naver.com>
2022-07-07 15:41:38 +0900

    작가입력과 등록구현

--- src/main/java/com/ll/LifeQuotes_SSG/App.java
+++ src/main/java/com/ll/LifeQuotes_SSG/App.java
@@ -1,12 +1,36 @@
 package com.ll.LifeQuotes_SSG;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class App {
-    public void run() {
-        System.out.println("== 명언 SSG ==");
-
+    public void run() throws IOException {
+        System.out.println("== 명언 SSG Commend==");
+        System.out.println("1. 등록 2. 목록 3. 수정 4. 삭제 ");
         Scanner sc = new Scanner(System.in);
+        System.out.println("\n");
+        String filePath = "D:/MyWork/Test.txt"; //파일생성
+
+        File file = new File(filePath); // File객체 생성
+        if(!file.exists()){ // 파일이 존재하지 않으면
+            file.createNewFile(); // 신규생성
+        }
+
+        // BufferedWriter 생성
+        BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+
+        // 파일에 쓰기
+        writer.write("하이루!");
+        writer.newLine();
+        writer.write("반가워!");
+        writer.newLine();
+
+        // 버퍼 및 스트림 뒷정리
+        writer.flush(); // 버퍼의 남은 데이터를 모두 쓰기
+        writer.close(); // 스트림 종료
 
         outer:
         while (true) {
@@ -15,14 +39,12 @@ public class App {
             switch (cmd) {
                 case "등록":
                     System.out.printf("명언) ");
-                    cmd = sc.nextLine().trim();
+                    String content = sc.nextLine().trim();
                     System.out.printf("작가) ");
-                    cmd = sc.nextLine().trim();
+                    String author = sc.nextLine().trim();
+                    System.out.println("1번 명언이 등록되었습니다.");
             }
 
-
-
-            System.out.println("1번 명언이 등록되었습니다.");
             switch (cmd) {
                 case "종료":
                     break outer;
@@ -32,3 +54,5 @@ public class App {
         sc.close();
     }
 }
+//파일 생성 조회 수정 삭제.
+//번호: id, 명언: content 작가: name
