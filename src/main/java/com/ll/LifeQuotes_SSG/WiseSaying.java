341201419ea7a8d1cae2dfdb985e03c8782eae31
castlehyeon <qnftktls118@naver.com>
2022-07-08 14:39:07 +0900

    미리 입력된 명령어를 받아들일 수 있는 스캐너 객체

---
+++ src/main/java/com/ll/LifeQuotes_SSG/WiseSaying.java
@@ -0,0 +1,12 @@
+package com.ll.LifeQuotes_SSG;
+
+public class WiseSaying {
+    int id;
+    String content;
+    String author;
+    public WiseSaying(int id, String content, String author) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+}
