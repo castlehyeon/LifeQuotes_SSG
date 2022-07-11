c58a95b64d3dba4c78bdb48d04b9b7daa6bb1756
castlehyeon <qnftktls118@naver.com>
2022-07-11 14:46:55 +0900

    입력받은 내용을 저장, 목록기능 구현

--- src/main/java/com/ll/LifeQuotes_SSG/App.java
+++ src/main/java/com/ll/LifeQuotes_SSG/App.java
@@ -1,37 +1,48 @@
 package com.ll.LifeQuotes_SSG;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public void run() {
-        System.out.println("== 명언 SSG Commend==");
-        System.out.println("1. 등록 2. 목록 3. 수정 4. 삭제 ");
+        System.out.println("== 명언 SSG ==");
         Scanner sc = new Scanner(System.in);
-        System.out.println("\n");
 
-        int lastId = 0; //마지막글번호 인덱스
+        // 가장 마지막 명언글의 번호
+        List<WiseSaying> wiseSayings = new ArrayList<>();
+        int wiseSayingLastId = 0;
+
         outer:
         while (true) {
             System.out.printf("명령) ");
             String cmd = sc.nextLine().trim();
             switch (cmd) {
                 case "등록":
-                    System.out.printf("명언) ");
+                    System.out.printf("명언 : ");
                     String content = sc.nextLine().trim();
-                    System.out.printf("작가) ");
+                    System.out.printf("작가 : ");
                     String author = sc.nextLine().trim();
-                    int id = ++lastId;
+                    int id = ++wiseSayingLastId; // 명언 글 번호 증가
+
                     WiseSaying wiseSaying = new WiseSaying(id, content, author);
                     System.out.println(wiseSaying);
-                    System.out.printf("%d번 명언이 등록되었습니다.", id);
-            }
+                    wiseSayings.add(wiseSaying);
 
-            switch (cmd) {
+                    System.out.printf("%d번 명언이 등록되었습니다.\n", id);
+                    break;
+                case "목록":
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("-------------------");
+                    for (int i = wiseSayings.size() - 1; i >= 0; i--) {
+                        WiseSaying wiseSaying_ = wiseSayings.get(i);
+                        System.out.printf("%d / %s / %s\n", wiseSaying_.id, wiseSaying_.content, wiseSaying_.author);
+                    }
+                    break;
                 case "종료":
                     break outer;
             }
         }
-
         sc.close();
     }
 }
