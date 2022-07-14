0125c961ab8dd83d26737fe33ddf2c866e871e66
castlehyeon <qnftktls118@naver.com>
2022-07-14 15:30:36 +0900

    수정기능구현

--- src/main/java/com/ll/LifeQuotes_SSG/App.java
+++ src/main/java/com/ll/LifeQuotes_SSG/App.java
@@ -34,6 +34,9 @@ public class App {
                 case "삭제":
                     remove(rq);
                     break;
+                case "수정":
+                    modify(rq);
+                    break;
                 case "목록":
                     list(rq);
                     break;
@@ -42,6 +45,38 @@ public class App {
             }
         }
         sc.close();
+    }
+
+    private void modify(Rq rq) {
+        //URL에 입력된 id 얻기
+        int paramId = rq.getIntParam("id", 0);
+
+        //URL에 입력된 id가 없다면 작업중지
+        if( paramId == 0 ) {//아이디가 있는지 먼저 검사.
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
+
+        //URL에 입력된 id에 해당하는 명언객체 찾기
+        WiseSaying foundwiseSaying = finById(paramId); //지우려는 객체
+        //id로 검색하는 것은 따로 빼놓을 것임.
+
+
+        if( foundwiseSaying == null ) {
+            System.out.printf("%d번 명언은 존재하지 않습니다.\n", paramId);
+            return;
+        }
+
+        //수정
+        //기존내용 출력
+        System.out.printf("명언(기존) : %s\n", foundwiseSaying.content);
+        System.out.printf("명언 : ");
+        foundwiseSaying.content = sc.nextLine();
+        System.out.printf("작가(기존) : %s\n", foundwiseSaying.author);
+        System.out.printf("명언 : ");
+        foundwiseSaying.author = sc.nextLine();
+
+        System.out.printf("%d번 명언이 수정되었습니다.\n", paramId);
     }
 
     private void list(Rq rq) {
--- src/main/java/com/ll/LifeQuotes_SSG/Rq.java
+++ src/main/java/com/ll/LifeQuotes_SSG/Rq.java
@@ -1,45 +1,52 @@
 package com.ll.LifeQuotes_SSG;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Rq {
-    String queryStr;
     String path;
     public String url;
+    Map<String, String> queryParams;
     public Rq(String url) {
         this.url = url;
         String[] urlBits = url.split("\\?", 2);
         this.path = urlBits[0];
 
+        queryParams = new HashMap<>();
+
         if ( urlBits.length == 2 ) {
-            this.queryStr = urlBits[1];
+            String queryStr = urlBits[1];
+
+            String[] paramBits = queryStr.split("&");
+
+            for( String paramBit : paramBits ) {
+                String[] paramNameAndValue = paramBit.split("=",2);
+
+                if ( paramNameAndValue.length == 1 ) {
+                    continue;
+                }//만약 value값에 아무것도 들어오지 않았다면?
+
+                String paramName = paramNameAndValue[0].trim();
+                String paramValue = paramNameAndValue[1].trim();
+
+                queryParams.put(paramName, paramValue);
+            }
         }
         //"?"를 기준으로 2덩어리로 나눌 것.
-
     }
 
     public int getIntParam(String paramName, int defaultValue) {
-
-
-        //?뒤에 내용이 아예없다면.
-        if( queryStr == null ) {// 뒤에 내용에 해당하는 쿼리스트링이 비어있다면,
+        if ( queryParams.containsKey(paramName) == false){// containskey는 안에 paraName이 있는지 확인하는 메서드
             return defaultValue;
         }
-        String[] bits = queryStr.split("&");
-        //나눠진 두 덩어리 중 뒤에를 & 기준으로 나눈다.
 
-        for (String urlBit : bits){//향상된 for문
-            String[] paramNameAndValue = urlBit.split("=",2);
-            //파라미터를 name과 value로 나누는 작업
-            String paramName_ = paramNameAndValue[0];
-            String paraValue = paramNameAndValue[1];
+        String paramValue = queryParams.get(paramName);
 
-            if( paramName.equals(paramName_)){
-                return Integer.parseInt(paraValue);
-                //반환형이 Int이기 때문에 정수화를 시킨다.
-            }
+        if ( paramValue.length() == 0){
+            return defaultValue;
         }
-        return defaultValue;
+        return Integer.parseInt(queryParams.get(paramName));
     }
 
     public String getPath() {
