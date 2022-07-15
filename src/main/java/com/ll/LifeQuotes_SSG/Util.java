70bc7c23d61e9a8a7cb011cd5a15439f49384998
castlehyeon <qnftktls118@naver.com>
2022-07-15 13:12:58 +0900

    try with resources 사용하여, 파일관련자원 자동해제

---
+++ src/main/java/com/ll/LifeQuotes_SSG/Util.java
@@ -0,0 +1,41 @@
+package com.ll.LifeQuotes_SSG;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class Util {
+    public static void saveToFile(String path, String body) {
+        try(RandomAccessFile stream = new RandomAccessFile(path, "rw");
+            FileChannel channel = stream.getChannel()){
+            byte[] strBytes = body.getBytes();
+            ByteBuffer buffer = ByteBuffer.allocate(strBytes.length);
+            buffer.put(strBytes);
+            buffer.flip();
+            channel.write(buffer);
+//            stream.close();
+//            channel.close();
+            //구문생략하는 방법->해당 객체를 try로 빼내면 된다.: try with resources 사용
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void mkdir(String path) {
+        File dir = new File(path);
+        dir.mkdir();
+    }
+
+    public static String getFromFile(String path) {
+        try (RandomAccessFile reader = new RandomAccessFile(path, "r")){
+            String body = reader.readLine();
+            //reader.close();
+            return body;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
